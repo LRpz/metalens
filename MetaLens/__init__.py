26b11f7bb750013f5d531e2a0cd1139b5582f07b
Luca Rappez <rappez@embl.de>
2024-12-12 12:16:07 +0100

    refactor: simplify package structure - Remove nested MetaLens directory - Update package configuration - Maintain same functionality with cleaner structure

--- MetaLens/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-MetaLens: Super-Resolved Spatial Metabolomics
-"""
-
-__version__ = '0.1.0' 
--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='metalens',
     version='0.1.0',
     description='MetaLens: Super-Resolved Spatial Metabolomics',
-    packages=find_packages(),
+    packages=['MetaLens'],
     python_requires='>=3.8',
     install_requires=[
         'numpy',
